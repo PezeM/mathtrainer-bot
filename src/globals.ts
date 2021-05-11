a05e3815b8838a086bedb3f68b9525379b72506f
PezeM <16854655+PezeM@users.noreply.github.com>
2021-05-11 14:38:00 +0200

    Fixed puppeteer import

--- src/globals.ts
+++ src/globals.ts
@@ -1,1 +1,2 @@
 export const SITE_URL = "https://www.mathtrainer.org/";
+export const START_BUTTON_SELECTOR = '.start.is-armed';
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,68 @@
-/**
- * Some predefined delays (in milliseconds).
- */
-import puppeteer from 'puppeteer/lib/cjs/puppeteer/node-puppeteer-core';
+import * as puppeteer from 'puppeteer';
 import { SITE_URL } from './globals';
+import { wait } from './helpers/time';
+import { Page } from 'puppeteer';
 
-async function browser() {
+async function start(page: Page) {
+  const selector = '.start.is-armed';
+  await page.waitForSelector(selector);
+  await page.click(selector);
+
+  async function resolve(iteration = 1) {
+    if (iteration >= 5) return;
+
+    const numbersSelector = '.numbers';
+    const operatorSelector = '.operator';
+
+    await Promise.all([
+      page.waitForSelector(numbersSelector),
+      page.waitForSelector(operatorSelector),
+    ]);
+
+    console.log('After waiting for selectors');
+
+    const values = await page.evaluate(() => {
+      const numbers = document.querySelector('.numbers');
+      const divs = [...numbers.querySelectorAll('div')];
+      return divs.map((value) => value.innerText);
+    });
+
+    console.log('values', values);
+
+    const operator = await page.$eval(operatorSelector, (el) => el.innerHTML);
+    console.log('operator', operator);
+
+    const numberValues = values.map((v) => parseInt(v));
+
+    let result = 0;
+    switch (operator) {
+      case '+':
+        result = numberValues[0] + numberValues[1];
+        break;
+      case '-':
+        result = numberValues[0] - numberValues[1];
+        break;
+      case '*':
+        result = numberValues[0] * numberValues[1];
+        break;
+      case '/':
+        result = numberValues[0] / numberValues[1];
+        break;
+      default:
+        console.log('Wrong operator', operator);
+        break;
+    }
+
+    console.log('result', result);
+    await page.keyboard.type(result.toString());
+    await wait(1000);
+    await resolve(++iteration);
+  }
+
+  await resolve(1);
+}
+
+async function startBrowser() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   page.on('console', (msg) => {
@@ -13,54 +71,8 @@ async function browser() {
 
   await page.goto(SITE_URL);
 
-  const selector = '.start.is-armed';
-  await page.waitForSelector(selector);
-  await page.click(selector);
-
-  const numbersSelector = '.numbers';
-  const operatorSelector = '.operator';
-
-  await Promise.all([
-    page.waitForSelector(numbersSelector),
-    page.waitForSelector(operatorSelector)
-  ]);
-
-  console.log('After waiting for selectors');
-
-  const values = await page.evaluate(() => {
-    const numbers = document.querySelector('.numbers');
-    const divs = [...numbers.querySelectorAll('div')];
-    return divs.map(value => value.innerText);
-  });
-
-  console.log('values', values);
-
-  const operator = await page.$eval(operatorSelector, el => el.innerHTML);
-  console.log('operator', operator);
-
-  const numberValues = values.map(v => parseInt(v));
-
-  let result = 0;
-  switch (operator) {
-    case '+':
-      result = numberValues[0] + numberValues[1];
-      break;
-    case '-':
-      result = numberValues[0] - numberValues[1];
-      break;
-    case '*':
-      result = numberValues[0] * numberValues[1];
-      break;
-    case '/':
-      result = numberValues[0] / numberValues[1];
-      break;
-    default:
-      console.log('Wrong operator', operator);
-      break;
-  }
-
-  console.log('result', result);
-  await page.keyboard.type(result.toString());
+  await start(page);
 }
 
-browser();
+
+startBrowser();
